4ed9439a2643b1a2593777a177f3833825d4266b
XByDesign <90591097+Xbydesign1@users.noreply.github.com>
2021-12-08 12:02:33 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,45 @@
+$(document).ready(function(){
+  $(window).scroll(function(){
+    if(this.scrollY > 20){
+      $('.navbar').addClass("sticky");
+    }else{
+      $('.navbar').removeClass("sticky");
+    }
+  })
+});
+// typing text animation script
+  var typed = new Typed(".typing", {
+      strings: ["Artist", "Desginer", "Photographer"],
+      typeSpeed: 100,
+      backSpeed: 60,
+      loop: true
+  });
+  var typed = new Typed(".typing-2", {
+      strings: ["Artist", "Designer", "Photographer"],
+      typeSpeed: 100,
+      backSpeed: 60,
+      loop: true
+  });
+  // owl carousel script
+  $('.carousel').owlCarousel({
+      margin: 20,
+      loop: true,
+      autoplay: true,
+      autoplayTimeOut: 2000,
+      autoplayHoverPause: true,
+      responsive: {
+          0:{
+              items: 1,
+              nav: false
+          },
+          600:{
+              items: 2,
+              nav: false
+          },
+          1000:{
+              items: 3,
+              nav: false
+          }
+      }
+  });
+});
